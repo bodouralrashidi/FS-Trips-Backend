b189338140fbbc7599e42f87b1f646be0da7ad16
0xglk <abdullah0305@gmail.com>
2022-06-20 09:22:49 +0300

    Doing User Stuff almost Done

--- App.js
+++ App.js
@@ -1,4 +1,36 @@
-const express = require("express");
-const connectDB = require("./database");
+const express = require('express');
+const app = express();
+const passport = require("passport");
+const cors = require("cors");
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
-connectDB()
+const connectDB = require("./database");
+const userRoutes = require("./api/users/users.routes");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+//user Routes
+app.use(userRoutes);
+
+//error handling middleware
+app.use((err, req, res, next) =>{
+    res.status(err.status || 500 ).json({message: err.message || "Internal Server Error"});
+  })
+
+//Path not found
+app.use((req, res, next) =>{
+    res.status(404).json({message: "path not found"});
+    // res.redirect("/api/events/all")
+  })
+  
+connectDB();
+
+//Backend on Localhost:8000
+app.listen(8000, () => {
+    console.log('The application is running on localhost:8000');
+  });
