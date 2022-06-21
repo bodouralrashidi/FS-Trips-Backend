50bd953f636b760ef3d79cd3527333315fcd653a
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-06-21 05:39:39 +0300

    Add userId field to trip model

--- models/Trip.js
+++ models/Trip.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const TripSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  //   user_id: {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: "User",
-  //   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
   description: String,
   image: String,
   location: String,
